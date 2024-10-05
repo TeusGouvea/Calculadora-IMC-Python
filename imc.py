9f25d4d0f3964dfa97b990fd6548d2b612647e4c
Matheus Gouvêa <145941871+TeusGouvea@users.noreply.github.com>
2024-10-05 16:48:30 -0300

    imc.py
    Calculadora de IMC desenvolvida em python, trabalho de Logica de Programação.

---
+++ imc.py
@@ -0,0 +1,22 @@
+peso = float(input('Qual é o seu peso? (KG)'))
+
+altura = float(input('Qual é sua altura? (M)'))
+
+imc = peso / (altura ** 2)
+
+print('O IMC dessa pessoa é de {:.1f}'.format(imc))
+
+if imc < 18.5:
+    print('Você está abaixo do peso normal')
+
+elif  18.5 <= imc <25:
+    print('Parabéns você está na faixa de peso normal')
+
+elif 25 <= imc < 30:
+    print('Você está em Sobrepeso')
+
+elif 30 <= imc < 40:
+    print('Você está em Obesidade, Cuidado!')
+    
+elif imc >= 40:
+    print('Você está em Obesidade Mórbida, Cuidado!')
